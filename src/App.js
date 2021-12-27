1a86c6de317fa7b5ef1369b69ca6a4b2aca8d687
Piyush <piyushparate1@gmail.com>
2021-12-27 20:00:57 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-function _App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
 function App(props) {
   return (
     <div className="todoapp stack-large">
@@ -120,6 +99,5 @@ function App(props) {
     </div>
   );
 }
-
 
 export default App;
