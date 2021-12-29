9b8d29ad833c1f5727f75bad85829f1217ae639a
Piyush <piyushparate1@gmail.com>
2021-12-29 15:29:42 +0530

    implement filter

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) 
 {
   function addTask(name) {
@@ -46,16 +54,29 @@ function App(props)
 
   const [tasks, setTasks] = useState(props.tasks);
 
-  const taskList = 
-    tasks.map(task => (
-      <Todo 
-        id={task.id} 
-        name={task.name} 
+  const [filter, setFilter] = useState('All');
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map(task => (
+      <Todo
+        id={task.id}
+        name={task.name}
         completed={task.completed}
-        key={task.id} 
-        toggleTaskCompleted={toggleTaskCompleted} 
-        deleteTask = {deleteTask}
-        editTask = {editTask}/>
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
     ));
 
     const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
@@ -66,9 +87,7 @@ function App(props)
       <h1>Todo</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">
         {headingText}
