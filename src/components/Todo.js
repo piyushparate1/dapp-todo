fee4527ec09bf10ccae75b7212f01a86b0aa53d9
Piyush <piyushparate1@gmail.com>
2022-01-05 11:52:32 +0530

    fix todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-  export default class App extends React.Component {
-  
-    constructor(props) {
-      super(props);
-      this.state = {
-        isEditing: false,
-        newName: '',
-      }
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false,
+      newName: '',
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   handleChange(e) {
     this.setState({ newName: e.target.value });
@@ -22,45 +22,46 @@ import React from "react";
     this.setState({ newName: '' });
     this.setState({ isEditing: false });
   }
-  
 
-  editingTemplate = (
-<form className="stack-small" onSubmit={this.handleSubmit}>
-      <div className="form-group">
-        <label className="todo-label" htmlFor={this.props.id}>
-          New name for {this.props.name}
-        </label>
-        <input
-  id={this.props.id}
-  className="todo-text"
-  type="text"
-  value={this.newName}
-  onChange={this.handleChange}
-/>
-      </div>
-      <div className="btn-group">
-      <button
-        type="button"
-        className="btn todo-cancel"
-        onClick={() => this.setState({ isEditing: false })}
-      >
-          Cancel
-          <span className="visually-hidden">renaming {this.props.name}</span>
-        </button>
-        <button 
-          type="submit" 
-          className="btn btn__primary todo-edit"
-        >
-          Save
-          <span className="visually-hidden">new name for {this.props.name}</span>
-        </button>
-      </div>
-    </form>
-  );
-  
-  viewTemplate = (
-    <div className="stack-small">
-      <div className="c-cb">
+  render() 
+  {
+    var editingTemplate = (
+      <form className="stack-small" onSubmit={this.handleSubmit}>
+        <div className="form-group">
+          <label className="todo-label" htmlFor={this.props.id}>
+            New name for {this.props.name}
+          </label>
+          <input
+            id={this.props.id}
+            className="todo-text"
+            type="text"
+            value={this.newName}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn todo-cancel"
+            onClick={() => this.setState({ isEditing: false })}
+          >
+            Cancel
+            <span className="visually-hidden">renaming {this.props.name}</span>
+          </button>
+          <button
+            type="submit"
+            className="btn btn__primary todo-edit"
+          >
+            Save
+            <span className="visually-hidden">new name for {this.props.name}</span>
+          </button>
+        </div>
+      </form>
+    );
+
+    var viewTemplate = (
+      <div className="stack-small">
+        <div className="c-cb">
           <input
             id={this.props.id}
             type="checkbox"
@@ -72,21 +73,18 @@ import React from "react";
           </label>
         </div>
         <div className="btn-group">
-          <button type="button" className="btn" onClick={() => this.setState({ isEditing: true })}>
+          <button type="button" className="btn" onClick={()=>{ this.setState({ isEditing: true }); }}>
             Edit <span className="visually-hidden">{this.props.name}</span>
           </button>
           <button
             type="button"
             className="btn btn__danger"
-            onClick={() => this.props.deleteTask(this.props.id)}
-          >
+            onClick={() => this.props.deleteTask(this.props.id)}>
             Delete <span className="visually-hidden">{this.props.name}</span>
           </button>
         </div>
-    </div>
-  );
-
-  render() {
-    return <li className="todo">{this.isEditing ? this.editingTemplate : this.viewTemplate}</li>;
+      </div>
+    );
+    return <li className="todo">{this.state.isEditing ? editingTemplate : viewTemplate}</li>;
   }
 }
