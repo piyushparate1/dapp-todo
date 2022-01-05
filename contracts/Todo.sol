05aabfd45ce55cfc681389967b183c0064aaa7a0
Piyush <piyushparate1@gmail.com>
2022-01-05 18:16:22 +0530

    minor change

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -3,29 +3,36 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Todo 
 {
-  mapping(address => ToDoItem[]) private todoCollection;
+  mapping(address => ToDoItem[]) public todoList;
+  uint32 public todoCount;
 
   struct ToDoItem
   {
-    string content;
-    bool isCompleted;
+    uint32 id;
+    string name;
+    bool completed;
   }
 
   constructor() 
   {
-    todoCollection[msg.sender].push(ToDoItem("Hello World!!", false));
-    todoCollection[msg.sender].push(ToDoItem("Task #1", false));
-    todoCollection[msg.sender].push(ToDoItem("Task #2", false));
+    todoCount = 0;
+    Add('Eat');
+    Add('Code');
+    Add('Sleep');
   }
 
-  function Add(string calldata todoContent) public
+  function Add(string memory todoContent) public
   {
-    todoCollection[msg.sender].push(ToDoItem(todoContent, false));
+    todoList[msg.sender].push(ToDoItem(
+      todoCount++,
+      todoContent,
+      false
+    ));
   }
 
   function Get(address _address) public view
     returns (ToDoItem[] memory todolist)
   {
-    return todoCollection[_address];
+    return todoList[_address];
   }
 }
