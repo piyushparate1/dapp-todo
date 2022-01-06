bd5772909442b3a2415f0373ae86276e77ac5037
Piyush <piyushparate1@gmail.com>
2022-01-06 12:33:03 +0530

    draft component

---
+++ src/components/AppMetadata.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from '../config'
+
+
+export default class AppMetadata extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
