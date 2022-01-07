2ca41aa5d0f122526cc8555eeb85765e36416608
Piyush <piyushparate1@gmail.com>
2022-01-07 15:39:18 +0530

    remove context

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import ScratchPad from './components/ScratchPad';
 import reportWebVitals from './reportWebVitals';
-import { MainContextProvider } from "./contexts/MainContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainContextProvider value={{name : "dapp Todo"}}>
-      <ScratchPad />
+      {/* <ScratchPad /> */}
       <App />
-    </MainContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
