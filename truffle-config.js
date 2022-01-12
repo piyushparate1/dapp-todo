af91859c640bc6526031d0f8c4bd2687e6e1f7bc
Piyush <piyushparate1@gmail.com>
2022-01-12 19:56:53 +0530

    setup ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
+  contracts_build_directory: "./src/contracts",
+
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
-    },
-    loc_development_development: {
-      network_id: "*",
-      port: 8545,
-      host: "127.0.0.1"
     }
+  },
+  ropsten: {
+    provider: () => new HDWalletProvider(mnemonic, `wss://mainnet.infura.io/ws/v3/ab356fb07337451c8284f004c0d054ae`),
+    network_id: 3,       // Ropsten's id
+    gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
   },
   mocha: {},
   compilers: {
